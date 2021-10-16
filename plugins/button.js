2e8b65d75a3c259e93ea9023743d3bc4ec9eee8b
KangSulap <92285448+KangSulap@users.noreply.github.com>
2021-10-16 01:12:51 -0700

    Add files via upload

---
+++ plugins/button.js
@@ -0,0 +1,58 @@
+let handler = async (m, { conn }) =>
+conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
+                    "listMessage":  {
+                        "title": "HALO KAK\nSaya Bot Devin`\nPilih Menu Di Bawah Yaa\nDan Jangan Lupa Baca Dibawah ini\n\nSpam = Banned\nTelp = Blok\n\n\n\n\nLu semua ngntd\nGa paham gw pukul lu mek",
+                        "description": "*©Ngapain dibaca?🅥*",
+                        "buttonText": "Semoga kalian sehat-sehat ya bang:>🅥",
+                        "listType": "SINGLE_SELECT",
+                        "sections": [
+                            {
+                             "rows": [
+                                    {
+                                        "title": `Download Menu`,
+                                        "description": "\nIni menu buat download ye ajg 🅥",
+                                        "rowId": ".downloadmenu"
+                                    },{
+                                        "title": "Database Menu",
+                                        "description": "\nIni au dh fungsinya apa:v 🅥",
+                                        "rowId": ".dbmenu"                 
+                                    },{
+                                        "title": "Button Menu",
+                                        "description": "\nKlo ini buat anu 🅥",
+                                        "rowId": ".button"                 
+                                    },{
+                                    	"title": "All Menu",
+                                        "description": "\nIni all menu ya mek 🅥",
+                                        "rowId": ".allmenu"                 
+                                    },{
+                                        "title": "Jadibot Menu",
+                                        "description": "\nIni buat jadi bot sementata 🅥",
+                                        "rowId": ".jadibotmenu"                 
+                                    },{
+                                        "title": "Owner Menu",
+                                        "description": "\nIni mo liat owner/yang bikin bot 🅥",
+                                        "rowId": ".ownermenu"                 
+                                    },{
+                                        "title": "Hentai Menu",
+                                        "description": "\nIni buat wibu 🅥",
+                                        "rowId": ".hentaimenu"                 
+                                    },{
+                                        "title": "Absen Menu",
+                                        "description": "\nIni buat absen kek anak tk 🅥",
+                                        "rowId": ".absenmenu"                 
+                                    }, {
+                                        "title": " Group Setting",
+                                        "description": "\nIni khusus grup ye anjnc 🅥",
+                                        "rowId": ".groupset"
+                                    }
+                                ]
+                            }
+                        ], "contextInfo": { "stanzaId": m.key.id,
+"participant": m.sender,
+"quotedMessage": m.message
+}
+                    }
+                 }, {}), {waitForAck: true})
+handler.command = ['menu']
+handler.register = true
+module.exports = handler
