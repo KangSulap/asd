09f7bfd2f69d3310bf782f87655401e7c309995c
KangSulap <92285448+KangSulap@users.noreply.github.com>
2021-10-19 16:06:15 +0700

    Delete owner.js

--- plugins/owner.js
+++
@@ -1,9 +0,0 @@
-function handler(m) {
-  // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6283144570377', 'RakuChan', m)
-stc = conn.sendContact(m.chat, '6285694982557', 'DpinBot', m)
-  conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Developer DpinBot*', 'status@broadcast')
-}
-handler.command = ['owner']
-
-module.exports = handler
